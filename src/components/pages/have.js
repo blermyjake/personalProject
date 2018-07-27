25207684021c3338cafe3334ac3ce9f64e0f48a7
blermyjake <jeremy73120@gmail.com>
2018-07-27 15:54:06 -0500

    header grey

--- src/components/pages/have.js
+++ src/components/pages/have.js
@@ -1,59 +1,46 @@
-import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
-
-
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 // import Heart from './heart';
 // import Time from './time';
 // import Donations from './donations';
 // import Skills from './skills';
-import '../../Assets/css/default.min.css';
+import "../../Assets/css/default.min.css";
 
 class Have extends Component {
-    render() {
-        return (
-            <div className="container-fluid">
-                
-                <h1>
-                Get Involved
-                </h1>
-                <h3>Inspire change in your community.</h3>
-                
-                <div className="haveDiv">
+  render() {
+    return (
+      <div className="container-fluid">
+        <h1>Get Involved</h1>
+        <h3>Inspire change in your community.</h3>
 
-                        <button className="button">
-                            <Link to="/have/heart" >The Heart</Link>
-                        </button>
-                        
-                        
-                        <button className="button">
-                            <Link to="/have/skills" >Skills</Link>
-                        </button>
-                       
+        <div className="haveDiv">
+          <button className="button">
+            <Link to="/have/heart">The Heart</Link>
+          </button>
 
-                        <button className="button">
-                            <Link to="/have/donations" >Dontations</Link>
-                        </button>
-                        
+          <button className="button">
+            <Link to="/have/skills">Skills</Link>
+          </button>
 
-                        <button className="button">
-                            <Link to="/have/time" >Time</Link>
-                        </button>
-                        
+          <button className="button">
+            <Link to="/have/donations">Dontations</Link>
+          </button>
 
-                        
-                        
-                 
-                </div>
-               
-                
-                <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus, totam consequatur ipsa accusantium praesentium error consequuntur, modi ipsam atque asperiores nesciunt deleniti eligendi? Nam culpa ab ipsum omnis temporibus dicta.
-                </p>
+          <button className="button">
+            <Link to="/have/time">Time</Link>
+          </button>
+        </div>
 
-            </div>
-        );
-    }
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus,
+          totam consequatur ipsa accusantium praesentium error consequuntur,
+          modi ipsam atque asperiores nesciunt deleniti eligendi? Nam culpa ab
+          ipsum omnis temporibus dicta.
+        </p>
+      </div>
+    );
+  }
 }
 
 export default Have;
--- src/components/pages/homePage.js
+++ src/components/pages/homePage.js
@@ -1,18 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class HomePage extends Component {
-    render() {
-        return (
-            <div className="container-fluid">
-            <h1>
-                Home Page Content
-            </h1>  
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="homeTop">
+          <h1>Home Page Content</h1>
 
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur, in suscipit eos dolor expedita assumenda dignissimos vero quo possimus cupiditate enim atque id sed ab minus nobis repellendus voluptas. Fugit!</p>  
-            </div>
-        );
-    }
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Consequuntur, in suscipit eos dolor expedita assumenda dignissimos
+            vero quo possimus cupiditate enim atque id sed ab minus nobis
+            repellendus voluptas. Fugit!
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default HomePage;
