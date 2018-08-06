8d2fad87ae1c44849c35a0c2af75063e29105bea
blermyjake <jeremy73120@gmail.com>
2018-08-06 16:08:03 -0500

    hamburger smooth

--- src/components/headerComponent/header.js
+++ src/components/headerComponent/header.js
@@ -3,43 +3,88 @@ import { Link } from "react-router-dom";
 import hands from "../headerComponent/hands.png";
 
 class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isOpened: false
+    };
+  }
   render() {
     // console.log(process.env.REACT_APP_LOGIN)
     return (
-      <header>
-        {/* <div className="logo">HELPING HANDS</div> */}
-        <img src={hands} className="logo" />
-        <nav>
-          <label for="toggle">&#9776;</label>
-          <input type="checkbox" id="toggle" />
-          <ul className="ulStyle">
-            <li className="Home">
-              <Link to="/">Home</Link>
-              {/* <a href="#">Home</a> */}
-            </li>
-            <li className="Need">
-              <Link to="/need">Get Help</Link>
-              {/* <a href="#">Products</a> */}
-            </li>
-            <li className="Have">
-              <Link to="/have">Give Help</Link>
-              {/* <a href="#">Contact</a> */}
-            </li>
-            <li className="About">
-              <Link to="/about">About</Link>
-              {/* <a href="#">Contact</a> */}
-            </li>
+      <div>
+        <header>
+          {/* <div className="logo">HELPING HANDS</div> */}
+          <img src={hands} className="logo" />
+          <nav>
+            <label
+              for="toggle"
+              onClick={() => this.setState({ isOpened: !this.state.isOpened })}
+            >
+              &#9776;
+            </label>
+            <input type="checkbox" id="toggle" />
+            <section
+              className={
+                this.state.isOpened
+                  ? "drop-down-menu opened"
+                  : "drop-down-menu closed"
+              }
+            >
+              <ul className="ulStyle">
+                <li className="Home">
+                  <Link to="/" className="header-links">
+                    Home
+                  </Link>
+                  {/* <a href="#">Home</a> */}
+                </li>
+                <li className="Need">
+                  <Link to="/need" className="header-links">
+                    Get Help
+                  </Link>
+                  {/* <a href="#">Products</a> */}
+                </li>
+                <li className="Have">
+                  <Link to="/have" className="header-links">
+                    Give Help
+                  </Link>
+                  {/* <a href="#">Contact</a> */}
+                </li>
+                <li className="About">
+                  <Link to="/about" className="header-links">
+                    About
+                  </Link>
+                  {/* <a href="#">Contact</a> */}
+                </li>
 
-            <li className="User">
-              <Link to="/user">Profile</Link>
-            </li>
-            <li className="Login">
-              <a href={process.env.REACT_APP_LOGIN}>Login</a>
-              {/* <a href="#">Contact</a> */}
-            </li>
-          </ul>
-        </nav>
-      </header>
+                <li className="User">
+                  <Link to="/user" className="header-links">
+                    Profile
+                  </Link>
+                </li>
+                <li className="Login">
+                  <a
+                    className="header-links"
+                    href={process.env.REACT_APP_LOGIN}
+                  >
+                    Login
+                  </a>
+                  {/* <a href="#">Contact</a> */}
+                </li>
+              </ul>
+            </section>
+          </nav>
+        </header>
+        {/* <section
+          className={
+            this.state.isOpened
+              ? "drop-down-menu opened"
+              : "drop-down-menu closed"
+          }
+        > */}
+        {/* <a>link 1</a>
+        </section> */}
+      </div>
     );
   }
 }
